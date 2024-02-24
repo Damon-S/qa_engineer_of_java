90d6eabcc83df72748cda34f8c4c6aa0c6043bd6
Ivan Mironov <rasseyaniy85@gmail.com>
2024-02-24 20:43:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//       Task_1
+//        System.out.println("Введите первую строку: ");
+//        String str1 = scanner.nextLine();
+//        System.out.println("Введите вторую строку: ");
+//        String str2 = scanner.nextLine();
+//        System.out.print("Выполняемое действие: ");
+//        if (str1.equals(str2)){
+//            System.out.print("Строки идентичны");
+//        } else {
+//            System.out.print("Строки не идентичны");
+//        }
+
+
+//       Task_2
+//        System.out.println("Введите первое число: ");
+//        int num1 = scanner.nextInt();
+//        System.out.println("Введите второе число: ");
+//        int num2 = scanner.nextInt();
+//        System.out.println("Выполняемое действие: ");
+//        String action = scanner.nextLine();
+//        action = scanner.nextLine();
+//        int res;
+//        Boolean match;
+//        switch (action) {
+//            case "+":
+//                res = num1 + num2;
+//                System.out.print("Результат: " + res);
+//                break;
+//            case "-":
+//                res = num1 - num2;
+//                System.out.print("Результат: " + res);
+//                break;
+//            case "*":
+//                res = num1 * num2;
+//                System.out.print("Результат: " + res);
+//                break;
+//            case "/":
+//                if(num2 == 0){
+//                    System.out.print("На ноль делить нельзя!");
+//                } else {
+//                    res = num1 / num2;
+//                    System.out.print("Результат: " + res);
+//                }
+//                break;
+//            case ">":
+//                match = num1 > num2;
+//                System.out.println("Результат: "  + num1 + ">" + num2);
+//                break;
+//            case "<":
+//                match = num1 < num2;
+//                System.out.println("Результат: " + num1 + "<" + num2);
+//                break;
+//            case "=":
+//                match = num1 == num2;
+//                System.out.println("Результат: " + num1 + " = " + num2);
+//                break;
+//            default:
+//                System.out.print("Ошибка");
+//        }
+
+//        Task_3
+//        byte[] nums = new byte[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        for (byte i = 0; i < nums.length; i++) {
+//            if (nums[i] % 2 == 0) {
+//                System.out.println(nums[i]);
+//            }
+//        }
+
+    }
+}
